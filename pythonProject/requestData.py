cc4e4d1659b8f4ac7247b5df850c20f360fdf29e
haggau15 <haggau94@gmail.com>
2024-02-12 15:45:05 +0100

    Added server to listen for request for more data from frontend

---
+++ pythonProject/requestData.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/requestdata', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        data = request.json  # Assuming JSON data is being sent
+        # Do something with the data
+        print("Received data:", data)
+        return "Data received successfully", 200
+    else:
+        return "Only POST requests are allowed", 405
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
